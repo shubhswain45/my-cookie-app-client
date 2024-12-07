f19c712d5e947283ce0668590c7d7c96ed04882b
shubhswain45 <shubhkantswain@.com>
2024-12-07 19:35:37 +0530

    good

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { useSetCookie } from "@/hooks";
 
-type Data = {
-  name: string;
-};
+
 
 const HomePage = () => {
 
-  const { mutateAsync: setCookie, isPending } = useSetCookie()
+  const { mutateAsync: setCookie } = useSetCookie()
 
   return (
    
